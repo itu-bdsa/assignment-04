107bc95393188fdd4acde9bbc0214af40051b3f9
oljh <oljh@itu.dk>
2022-10-04 12:38:22 +0200

    WorkItemRepository Code pasted

--- Assignment.Infrastructure/WorkItem.cs
+++ Assignment.Infrastructure/WorkItem.cs
@@ -6,6 +6,8 @@ public class WorkItem
 
     public string Title { get; set; }
 
+    public string Description { get; set; }
+
     public int? AssignedToId { get; set; }
 
     public User? AssignedTo { get; set; }
@@ -13,6 +15,9 @@ public class WorkItem
     public State State { get; set; }
 
     public ICollection<Tag> Tags { get; set; }
+
+    public DateTime Created { get; set; }
+    public DateTime StateUpdated { get; set; }
 
     public WorkItem(string title)
     {
--- Assignment.Infrastructure/WorkItemRepository.cs
+++ Assignment.Infrastructure/WorkItemRepository.cs
@@ -1,5 +1,145 @@
 namespace Assignment.Infrastructure;
 
-public class WorkItemRepository
+public class WorkItemRepository : IWorkItemRepository
 {
+    private readonly KanbanContext _context;
+
+    public WorkItemRepository(KanbanContext context)
+    {
+        _context = context;
+    }
+    
+    public (Response Response, int ItemId) Create(WorkItemCreateDTO item)
+    {
+        Response response;
+        var entity = new WorkItem(item.Title);
+        var assignedUser = _context.Users.FirstOrDefault(u => u.Id == item.AssignedToId);
+
+        if(assignedUser == null && item.AssignedToId != null) return (Response.BadRequest, 0);
+
+        ICollection<Tag> tags = new List<Tag>();
+        
+        entity.Description = item.Description;
+        entity.State = State.New;
+        entity.AssignedTo = assignedUser;
+        entity.Tags = tags;
+
+        var taskExists = _context.Items.FirstOrDefault(t => t.Id == entity.Id) != null;
+        
+        if(taskExists)
+        {
+            return (Response.Conflict, 0);
+        }
+
+        entity.Created = DateTime.UtcNow;
+        entity.StateUpdated = DateTime.UtcNow;
+
+        _context.Items.Add(entity);
+        _context.SaveChanges();
+
+        response = Response.Created;
+        
+
+        return (response, entity.Id);
+    }
+
+    public IReadOnlyCollection<WorkItemDTO> ReadRemoved()
+    {
+        return ReadByState(State.Removed);
+    }
+
+    public IReadOnlyCollection<WorkItemDTO> ReadByTag(string tag)
+    {
+        var tagQuery = from t in Read()
+            where t.Tags.Contains(tag)
+            select t ;
+
+        return tagQuery.Any() ? tagQuery.ToList() : null!;
+    }
+
+    public IReadOnlyCollection<WorkItemDTO> ReadByUser(int userId)
+    {
+        var userQuery = from t in Read() where t.Id == userId select t;
+        
+        return userQuery.Any() ? userQuery.ToList() : null!;
+    }
+
+    public IReadOnlyCollection<WorkItemDTO> ReadByState(State state)
+    {
+        var stateQuery = from t in Read() where t.State == state select t;
+        
+        return stateQuery.Any() ? stateQuery.ToList() : null!;
+    }
+    
+    public IReadOnlyCollection<WorkItemDTO> Read()
+    {
+        if(!_context.Items.Any())
+        {
+            return null!;
+        }
+
+
+        var tasks = from t in _context.Items
+            select new WorkItemDTO(t.Id, t.Title, t.AssignedTo.Name, t.Tags.Select(x => x.Name).ToList(), t.State);
+
+        return tasks.ToList();
+    }
+
+    public Response Update(WorkItemUpdateDTO workitem)
+    {
+        
+        var entity = _context.Items.Find(workitem.Id);
+        
+        if(entity == null) return Response.NotFound;
+        
+        var assignedUser = _context.Users.FirstOrDefault(u => u.Id == workitem.AssignedToId);
+
+        if(assignedUser == null) return Response.BadRequest;
+
+        entity.Title = workitem.Title;
+        entity.Description = workitem.Description;
+        entity.AssignedTo = assignedUser;
+        entity.Tags = workitem.Tags.Select(name => new Tag(name))
+            .ToList();
+        if(entity.State != workitem.State) entity.StateUpdated = DateTime.UtcNow;
+        
+        entity.State = workitem.State;
+        
+        _context.SaveChanges();
+        
+        return Response.Updated;
+    }
+
+    public Response Delete(int workItemId)
+    {
+        var workitem = _context.Items.FirstOrDefault(u => u.Id == workItemId);
+        
+        if(workitem == null)
+        {
+            return Response.NotFound;
+        }
+        
+        if(workitem.State == State.New) _context.Items.Remove(workitem);
+        else if (workitem.State == State.Active) {
+            workitem.State = State.Removed;
+            workitem.StateUpdated = DateTime.UtcNow;
+        }
+        else return Response.Conflict;
+        
+        return Response.Deleted;
+    }
+
+    public WorkItemDetailsDTO? Find(int workItemId)
+    {
+        var taskNotExists = _context.Items.FirstOrDefault(t => t.Id == workItemId) == null;
+
+        if (taskNotExists)
+        {
+            return null;
+        }
+        
+        var workitem = from t in _context.Items where t.Id == workItemId 
+        select new WorkItemDetailsDTO(t.Id, t.Title, t.Description, t.Created, t.AssignedTo.Name, t.Tags.Select(x => x.Name).ToList(), t.State, t.StateUpdated);
+        return workitem.First();
+    }
 }
